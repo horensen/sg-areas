39ee688e8587f22a25233c70af7b9563ac937a1e
horensen <5269379+horensen@users.noreply.github.com>
2021-06-17 13:53:20 +0800

    Add subzones in Area.BUKIT_BATOK

--- src/data/subzones.ts
+++ src/data/subzones.ts
@@ -61,6 +61,51 @@ const subzones: { [id in Subzone]: SubzoneDetails } = {
     coordinates: { latitude: 1.38128167548, longitude: 103.840062362 },
     area: Area.ANG_MO_KIO,
   },
+  [Subzone.BBT_BRICKWORKS]: {
+    name: { en: "Brickworks" },
+    coordinates: { latitude: 1.35565668063, longitude: 103.742218822 },
+    area: Area.BUKIT_BATOK,
+  },
+  [Subzone.BBT_BUKIT_BATOK_CENTRAL]: {
+    name: { en: "Bukit Batok Central" },
+    coordinates: { latitude: 1.34799446529, longitude: 103.750413276 },
+    area: Area.BUKIT_BATOK,
+  },
+  [Subzone.BBT_BUKIT_BATOK_EAST]: {
+    name: { en: "Bukit Batok East" },
+    coordinates: { latitude: 1.3491364514, longitude: 103.757537069 },
+    area: Area.BUKIT_BATOK,
+  },
+  [Subzone.BBT_BUKIT_BATOK_SOUTH]: {
+    name: { en: "Bukit Batok South" },
+    coordinates: { latitude: 1.34103935596, longitude: 103.758125977 },
+    area: Area.BUKIT_BATOK,
+  },
+  [Subzone.BBT_BUKIT_BATOK_WEST]: {
+    name: { en: "Bukit Batok West" },
+    coordinates: { latitude: 1.34748014212, longitude: 103.74225729 },
+    area: Area.BUKIT_BATOK,
+  },
+  [Subzone.BBT_GOMBAK]: {
+    name: { en: "Gombak" },
+    coordinates: { latitude: 1.36928786794, longitude: 103.757485597 },
+    area: Area.BUKIT_BATOK,
+  },
+  [Subzone.BBT_GUILIN]: {
+    name: { en: "Guilin" },
+    coordinates: { latitude: 1.35546396378, longitude: 103.753812023 },
+    area: Area.BUKIT_BATOK,
+  },
+  [Subzone.BBT_HILLVIEW]: {
+    name: { en: "Hillview" },
+    coordinates: { latitude: 1.3556494864, longitude: 103.763919292 },
+    area: Area.BUKIT_BATOK,
+  },
+  [Subzone.BBT_HONG_KAH_NORTH]: {
+    name: { en: "Hong Kah North" },
+    coordinates: { latitude: 1.36271213917, longitude: 103.748003786 },
+    area: Area.BUKIT_BATOK,
+  },
 };
 
 export default subzones;
