d45e4fe9b9b90a13322b74dbfa9fe812a8c901e9
horensen <5269379+horensen@users.noreply.github.com>
2021-06-20 13:42:40 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,26 +9,32 @@ import {
 
 export const administrativeLocations = { Region, Area, Subzone };
 
-export const getAreas = (regions: Region[]): { [id: string]: AreaDetails } => {
-  return Object.keys(data.areas.default)
-    .filter((id: string) =>
-      regions.includes(data.areas.default[id as Area].region),
-    )
-    .reduce((obj: { [id: string]: AreaDetails }, id: string) => {
-      obj[id] = data.areas.default[id as Area];
-      return obj;
-    }, {});
+export const getAreas = (regions?: Region[]): { [id: string]: AreaDetails } => {
+  if (regions && regions.length > 0) {
+    return Object.keys(data.areas.default)
+      .filter((id: string) =>
+        regions.includes(data.areas.default[id as Area].region),
+      )
+      .reduce((obj: { [id: string]: AreaDetails }, id: string) => {
+        obj[id] = data.areas.default[id as Area];
+        return obj;
+      }, {});
+  }
+  return data.areas.default;
 };
 
 export const getSubzones = (
-  areas: Area[],
+  areas?: Area[],
 ): { [id: string]: SubzoneDetails } => {
-  return Object.keys(data.subzones.default)
-    .filter((id: string) =>
-      areas.includes(data.subzones.default[id as Subzone].area),
-    )
-    .reduce((obj: { [id: string]: SubzoneDetails }, id: string) => {
-      obj[id] = data.subzones.default[id as Subzone];
-      return obj;
-    }, {});
+  if (areas && areas.length > 0) {
+    return Object.keys(data.subzones.default)
+      .filter((id: string) =>
+        areas.includes(data.subzones.default[id as Subzone].area),
+      )
+      .reduce((obj: { [id: string]: SubzoneDetails }, id: string) => {
+        obj[id] = data.subzones.default[id as Subzone];
+        return obj;
+      }, {});
+  }
+  return data.subzones.default;
 };
