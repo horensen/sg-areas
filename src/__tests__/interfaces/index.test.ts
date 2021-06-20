9055727446983d80a14977a6735840c9d9acd662
horensen <5269379+horensen@users.noreply.github.com>
2021-06-20 13:33:28 +0800

    Add test for no region duplicates

---
+++ src/__tests__/interfaces/index.test.ts
@@ -0,0 +1,25 @@
+import { Region, Area, Subzone } from "../../interfaces";
+
+describe("List of regions", () => {
+  it("should have no duplicates", () => {
+    const arr = Object.values(Region);
+    const hasUnique = new Set(arr).size === arr.length;
+    expect(hasUnique).toBe(true);
+  })
+})
+
+describe("List of planning areas", () => {
+  it("should have no duplicates", () => {
+    const arr = Object.values(Area);
+    const hasUnique = new Set(arr).size === arr.length;
+    expect(hasUnique).toBe(true);
+  })
+})
+
+describe("List of subzones", () => {
+  it("should have no duplicates", () => {
+    const arr = Object.values(Subzone);
+    const hasUnique = new Set(arr).size === arr.length;
+    expect(hasUnique).toBe(true);
+  });
+});
